52a682bad95cee3c8e937438ec26428ae7ae8658
Edgardo Vega <1819243+vegaed@users.noreply.github.com>
2018-11-05 08:31:35 -0500

    Initial project created by Spring Initializr

---
+++ src/main/java/com/ncc/neon/server/NeonServerApplication.java
@@ -0,0 +1,12 @@
+package com.ncc.neon.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeonServerApplication.class, args);
+	}
+}
