64fa6a73e666ba193a677c5f6d86af620f6b2413
Edgardo Vega <1819243+vegaed@users.noreply.github.com>
2019-01-15 08:07:23 -0500

    THOR-903 Fixing up the build and adding some things to make it easier to debug in vscode

---
+++ esadapter/src/main/java/com/ncc/neon/server/EsNeonServerApplication.java
@@ -0,0 +1,18 @@
+package com.ncc.neon.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+This class is completly unesssary but there is a bug in VSCode Spring-boot
+dashboard to start a project that has a main in a dependency.
+I filed an issue with the project tracking 
+https://github.com/Microsoft/vscode-spring-boot-dashboard/issues/63
+*/
+@SpringBootApplication()
+public class EsNeonServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsNeonServerApplication.class, args);
+	}
+}
