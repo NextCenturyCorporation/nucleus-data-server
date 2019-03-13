4d61bbcfbcc0d831ec395f4149394345e9436dd2
Rachel Artiss <rachel.artiss@nextcentury.com>
2019-03-13 16:23:55 -0400

    Resolve issue with filterservice/clearfilters endpoint.

--- server/src/main/java/com/ncc/neon/server/models/query/filter/FilterEvent.java
+++ server/src/main/java/com/ncc/neon/server/models/query/filter/FilterEvent.java
@@ -1,5 +1,8 @@
 package com.ncc.neon.server.models.query.filter;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,6 +12,9 @@ import lombok.experimental.Accessors;
 @AllArgsConstructor
 @NoArgsConstructor
 @Accessors(fluent = true)
+@JsonAutoDetect(getterVisibility=Visibility.NONE)
+// Added @JsonAutoDetect line because of issue with filterservice/clearfilters endpoint:
+// https://stackoverflow.com/questions/28466207/could-not-find-acceptable-representation-using-spring-boot-starter-web/28466881#28466881
 public class FilterEvent {
 
     /** a string description of the type of event */
