9347af0ff90e490ab2d64c23c50d8b2afdf7df7b
Thomas Schellenberg <thomas.schellenberg@nextcentury.com>
2019-08-05 10:36:26 -0400

    THOR-1374 Split error stack trace into separate readable lines.

--- server/src/main/java/com/ncc/neon/CustomErrorAttributes.java
+++ server/src/main/java/com/ncc/neon/CustomErrorAttributes.java
@@ -1,5 +1,6 @@
 package com.ncc.neon;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
@@ -16,6 +17,13 @@ public class CustomErrorAttributes extends DefaultErrorAttributes {
     @Override
     public Map<String, Object> getErrorAttributes(ServerRequest request, boolean includeStackTrace) {
         Map<String, Object> errorMap = super.getErrorAttributes(request, includeStackTrace);
+        String stackTrace = errorMap.get("trace").toString();
+        Map<String, String> stackTraceMap = new LinkedHashMap<String, String>();
+        String[] linesArray = stackTrace.split("\n");
+        for (int i = 0; i < linesArray.length; ++i) {
+            stackTraceMap.put("" + i, linesArray[i]);
+        }
+        errorMap.put("trace", stackTraceMap);
         return errorMap;
     }
 }
