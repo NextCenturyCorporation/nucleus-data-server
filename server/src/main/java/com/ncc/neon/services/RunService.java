0245c222e7f70cee196dbc4f5354a5a75dd410a0
chase-farmer <cfarmer@hq.nextcentury.com>
2020-01-29 12:33:01 -0500

    data notification for runs

--- server/src/main/java/com/ncc/neon/services/RunService.java
+++ server/src/main/java/com/ncc/neon/services/RunService.java
@@ -19,8 +19,8 @@ public class RunService extends ElasticSearchService<Run> {
     private static final String dataType = "run";
 
     @Autowired
-    RunService(@Value("${db_host}") String dbHost) {
-        super(dbHost, index, dataType, Run.class);
+    RunService(DatasetService datasetService, @Value("${db_host}") String dbHost) {
+        super(dbHost, index, dataType, Run.class, datasetService);
     }
 
     public Mono<Tuple2<String, RestStatus>> initRun(String configFile) {
@@ -33,32 +33,32 @@ public class RunService extends ElasticSearchService<Run> {
         data.put("train_end_time", DateUtil.getCurrentDateTime());
         data.put("status", RunStatus.INFERENCING);
         data.put("inf_start_time", DateUtil.getCurrentDateTime());
-        return update(data, runId);
+        return updateAndRefresh(data, runId);
     }
 
     public Mono<RestStatus> updateToScoringStatus(String runId) {
         Map<String, Object> data = new HashMap<>();
         data.put("inf_end_time", DateUtil.getCurrentDateTime());
         data.put("status", RunStatus.SCORING);
-        return update(data, runId);
+        return updateAndRefresh(data, runId);
     }
 
     public Mono<RestStatus> updateToDoneStatus(String completedRunId) {
         Map<String, Object> data = new HashMap<>();
         data.put("status", RunStatus.DONE);
-        return update(data, completedRunId);
+        return updateAndRefresh(data, completedRunId);
     }
 
     public Mono<RestStatus> updateToErrorStatus(String runId, String errorMsg) {
         Map<String, Object> data = new HashMap<>();
         data.put("status", RunStatus.ERROR);
         data.put("status_message", errorMsg);
-        return update(data, runId);
+        return updateAndRefresh(data, runId);
     }
 
     public Mono<RestStatus> updateOutputs(String runId, String outputsKey, String[] outputs) {
         Map<String, Object> data = new HashMap<>();
         data.put(outputsKey, outputs);
-        return update(data, runId);
+        return updateAndRefresh(data, runId);
     }
 }
