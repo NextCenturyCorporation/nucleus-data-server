543f4a0c0d9b669896a481aabae247ac5b0e9746
KBai <konnoke@live.com>
2020-06-05 13:33:45 -0400

    Revert "Removed Timestamp for dryRun"
    This reverts commit 4b81a12ad3d486d8999883f229252c60d3a5aed3.

--- server/src/main/java/com/ncc/neon/better/ExperimentConfig.java
+++ server/src/main/java/com/ncc/neon/better/ExperimentConfig.java
@@ -64,7 +64,7 @@ public class ExperimentConfig {
     public String getName() { return name; }
 
     private void initName() {
-        name = module + "_experiment_";
+        name = module + "_experiment_" + DateUtil.getCurrentDateTime();
     }
 
     private void parseConfig(String trainFile, String devFile) {
