7de5483a362b32a38ffd8a48e79f8d9ba11a0559
Darian <darian.garcia.404@gmail.com>
2020-09-11 11:11:10 -0400

    adding ES insertion into controller

--- server/src/main/java/com/ncc/neon/controllers/BetterController.java
+++ server/src/main/java/com/ncc/neon/controllers/BetterController.java
@@ -150,7 +150,7 @@ public class BetterController {
                     	for (String docID : queryResults) {
                     		String doc = ""; // introspect on documents to get the one corresponding to docID
                     		String uuid = ""; // introspect on documents to get the one corresponding to docID
-                    		storeInES(docID, doc, uuid);
+                    		IRDataService.initFile(docID, doc, uuid);
                     	}
                     	return queryResults;
                     });
